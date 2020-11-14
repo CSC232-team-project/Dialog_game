f1ec4e7a033d78ea240ea0ce985eaf1cd9ee0c41
VyHHoang <hoanghavy0604@gmail.com>
2020-11-14 17:04:28 -0500

    contructor

---
+++ ContainerItem.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ContainerItem extends Item {
+    
+    private ArrayList<Item> items;
+    
+    public ContainerItem(String name, String type, String description){
+        super(name, type, description);
+        items = new ArrayList<Item>();
+    }
+    
+    
+}
